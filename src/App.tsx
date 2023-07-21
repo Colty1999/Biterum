bfbdccbab5fd9a4c80c858090951f0f03ef569c4
Colty1999 <mateuszgietka99@gmail.com>
2023-07-21 19:09:01 +0200

    Particles background component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,19 @@
 import './App.scss'
-
 import Header from './layout/header/Header';
 import Home from './features/home/Home';
 import Footer from './layout/footer/Footer';
-import { useCallback } from 'react';
-import Particles from 'react-particles';
-import { loadSlim } from 'tsparticles-slim';
-import { Engine } from 'tsparticles-engine';
 import { Container } from '@mui/material';
+import ParticlesBackground from './modules/Particles/Particles';
 
 
 export default function App() {
 
-  const particlesInit = useCallback(async (engine: Engine) => {
-    await loadSlim(engine);
-  }, []);
-
   return (
     <Container>
-        <Particles
-          id="tsparticles"
-          init={particlesInit}
-          // loaded={particlesLoaded}
-          options={{
-            // background: {
-            //     color: {
-            //         value: "#0d47a1",
-            //     },
-            // },
-            zIndex: -99,
-            fpsLimit: 120,
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: "push",
-                },
-                onHover: {
-                  enable: true,
-                  mode: "repulse",
-                },
-                resize: true,
-              },
-              modes: {
-                push: {
-                  quantity: 4,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                },
-              },
-            },
-            particles: {
-              color: {
-                value: "#ffffff",
-              },
-              links: {
-                color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-              },
-              move: {
-                direction: "none",
-                enable: true,
-                outModes: {
-                  default: "bounce",
-                },
-                random: false,
-                speed: 2,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 80,
-              },
-              opacity: {
-                value: 0.5,
-              },
-              shape: {
-                type: "circle",
-              },
-              size: {
-                value: { min: 1, max: 5 },
-              },
-            },
-            detectRetina: true,
-          }}
-        />
-        <Header />
-        <Home />
-        <Footer />
+      <ParticlesBackground />
+      <Header />
+      <Home />
+      <Footer />
     </Container>
   )
 }
